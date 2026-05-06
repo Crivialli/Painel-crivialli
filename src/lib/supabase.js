e4772fd72262dbd1a5477b01dd8dfad8289192e0
Grupo Crivialli Brasil <49563934+Crivialli@users.noreply.github.com>
2026-05-06 08:56:23 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 // lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ootilzitdpayupyykcuj.supabase.co";
-const supabaseKey = "sb_publishable_3PEBQKv2shv9c6b7F4MVjQ_4Q3d6yRR";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
